1e8a9472a5c6c5b607ef58b50b9fccb1fbeb0526
unknown <sam.haff.rn@gmail.com>
2024-11-21 20:36:00 +0200

    script for test coverage

---
+++ scripts/run_tests_coverage.sh
@@ -0,0 +1,9 @@
+cd ../test_mongodb
+
+docker-compose up --detach --wait
+cd ..
+go test -coverprofile="coverage.out" -v ./...
+go tool cover -html="coverage.out" -o "coverage.html"
+cd ./test_mongodb
+docker-compose down
+cd ../scripts
