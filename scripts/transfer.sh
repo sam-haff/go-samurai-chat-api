9cc5247cfd9951902378e386c6ca91ce004be9ca
unknown <sam.haff.rn@gmail.com>
2024-11-27 10:34:48 +0200

    Fix ssh connection

--- scripts/transfer.sh
+++ scripts/transfer.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 ssh-keyscan -H $SERVER_ADDR >> ./known_hosts
-sshpass -P assphrase -p $SSH_KEY_PWD scp -r -i ssh-key.pem -o UserKnownHostsFile=./known_hosts ../images/* uploaduser@$SERVER_ADDR:/builds_upload/new/
+sshpass -P assphrase -p $SSH_KEY_PWD scp -v -r -i ssh-key.pem -o UserKnownHostsFile=./known_hosts ../images/* uploaduser@$SERVER_ADDR:/builds_upload/new/
--- scripts/transfer_i.sh
+++ scripts/transfer_i.sh
@@ -4,4 +4,4 @@ echo "do secure copy to the server..."
 echo "enter server addr"
 read server_addr
 
-scp -r -i ssh-key.pem ../images uploaduser@$server_addr:/builds_upload/
+scp -v -r -i ssh-key.pem ../images/* uploaduser@$server_addr:/builds_upload/new/
