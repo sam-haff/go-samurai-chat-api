24c1ea0b3858ddbbeca237b4086ec95bbde60528
unknown <sam.haff.rn@gmail.com>
2024-11-28 12:58:35 +0200

    Little docs change

--- scripts/build_and_transfer.sh
+++ scripts/build_and_transfer.sh
@@ -5,19 +5,19 @@
 # and upload them to the server using scp command.
 #
 # Requires:
-# - SERVER_BUILD_NUMBER env variable
+# - SERVER_BUILD_NUMBER environment variable
 # Incremental build version. Should be incremented for the new build.
-# - FIREBASE_CREDS_FILE env variable. 
+# - FIREBASE_CREDS_FILE environment variable. 
 # A path to firebase service account json. 
 # First you need a Firebase project with enabled Authentication.
 # Now get the file from Firebase console->Project settings->Service accounts->Generate new private key
-# - MONGODB_CONNECT_URL env variable
+# - MONGODB_CONNECT_URL environment variable
 # An mongodb connect url in with the credentails.
 # mongodb://<username>:<password>@<mongodb_url>/?<options>
 # If you use Atlas, you can find it by clicking Connect under your cluster,
 # choose Drivers then in any driver step 3 is the connection string which is
 # exactly the sought value.
-# - $SERVER_ADDR environment variable, which
+# - SERVER_ADDR environment variable, which
 # should contain ip address of the server.
 # - Server user <uploaduser> created as described in server_setup_readme.txt
 # - SSH key <ssh-key.pem> for the <uploaduser>, you get it from your
